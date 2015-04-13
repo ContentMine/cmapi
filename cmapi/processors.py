3cc20ea8e2470c9357d5e4f535ed7684960ba8d4
Mark MacGillivray <mark@cottagelabs.com>
2015-04-13 13:28:28 +0100

    adding processors

--- cmapi/processors.py
+++ cmapi/processors.py
@@ -281,7 +281,7 @@ class Amispecies(Processor):
                             doc = {}
                             doc["pre"] = result.get("pre")
                             doc["exact"] = result.get("exact")
-                            doc["fact"] = result.get("fact")
+                            doc["fact"] = result.get("name")
                             doc["post"] = result.get("post")
                             doc["name"] = result.get("name")
                             self.output['facts'].append(doc)
